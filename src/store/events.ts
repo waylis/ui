5523505ec3d16407534893388c6080a765096f0b
cheatsnake <72691412+cheatsnake@users.noreply.github.com>
2025-09-03 19:45:08 +0300

    feat: heartbeat tracking

--- src/store/events.ts
+++ src/store/events.ts
@@ -6,30 +6,52 @@ type EventHandler = (event: MessageEvent) => void;
 
 interface EventSourceStore {
     eventSource?: EventSource;
+    lastHeartbeatAt: Date;
+    heartbeatIntervalID?: number;
+
+    open: () => EventSource;
     addEventHandler: (eventName: string, handler: EventHandler) => void;
-    closeEventSource: () => void;
+    close: () => void;
 }
 
 export const useEventSourceStore = create<EventSourceStore>((set, get) => ({
     eventSource: undefined,
+    lastHeartbeatAt: new Date(),
+
+    open() {
+        get().eventSource?.close();
+        const eventSource = new EventSource(api.endpoint("events"));
+
+        eventSource.onerror = () => {
+            errNotify("Unable to connect to the server. Please try again later.");
+        };
+
+        eventSource.addEventListener("heartbeat", () => {
+            set({ lastHeartbeatAt: new Date() });
+        });
+
+        const heartbeatIntervalID = setInterval(() => {
+            if (get().eventSource && new Date().getTime() - get().lastHeartbeatAt.getTime() > 10_000) {
+                errNotify("Connection to the server lost. Try reloading the page.");
+                clearInterval(heartbeatIntervalID);
+            }
+        }, 2000);
+
+        set({ eventSource, heartbeatIntervalID });
+        return eventSource;
+    },
 
     addEventHandler: (eventName: string, handler: EventHandler) => {
         let { eventSource } = get();
 
         if (!eventSource) {
-            eventSource = new EventSource(api.endpoint("events"));
-
-            eventSource.onerror = () => {
-                errNotify("Event source connection error. Try reload a page.");
-            };
-
-            set({ eventSource });
+            eventSource = get().open();
         }
 
         eventSource.addEventListener(eventName, handler);
     },
 
-    closeEventSource: () => {
+    close: () => {
         const { eventSource } = get();
         if (eventSource) {
             eventSource.close();
