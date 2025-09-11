4d052ab57a97969b6e47d243498eaaaf1b196322
cheatsnake <72691412+cheatsnake@users.noreply.github.com>
2025-09-11 21:43:43 +0300

    chore: update proxy port

--- vite.config.ts
+++ vite.config.ts
@@ -3,10 +3,10 @@ import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
-    plugins: [react()],
-    server: {
-        proxy: {
-            "/api": { target: "http://localhost:7331", changeOrigin: true },
-        },
+  plugins: [react()],
+  server: {
+    proxy: {
+      "/api": { target: "http://localhost:7770", changeOrigin: true },
     },
+  },
 });
