4eda1f27422ae1e0ab7ff63e675814f434ea5af3
cheatsnake <72691412+cheatsnake@users.noreply.github.com>
2025-09-26 20:38:38 +0300

    chore: split chunks

--- vite.config.ts
+++ vite.config.ts
@@ -4,9 +4,21 @@ import react from "@vitejs/plugin-react";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+
   server: {
     proxy: {
       "/api": { target: "http://localhost:7770", changeOrigin: true },
+    },
+  },
+
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          recharts: ["recharts"],
+          mantineCharts: ["@mantine/charts"],
+        },
+      },
     },
   },
 });
